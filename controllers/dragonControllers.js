3fcde299d0ac000780cf670b0d9c9a3734ae6fca
Charlotte Prevost <chaprevost91@gmail.com>
2018-10-14 13:27:29 -0500

    Added/required controller

---
+++ controllers/dragonControllers.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+// -------------------- REQUIRE MODEL -------------------- //
+const Dragons = require('../models/dragonModel.js');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
