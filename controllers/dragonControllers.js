5ba04e1da91417f5aa6ce0255834707f89f7667c
Charlotte Prevost <chaprevost91@gmail.com>
2018-10-14 14:48:56 -0500

    Corrected concat in consoleLog - info now logs properly and is added to db

--- controllers/dragonControllers.js
+++ controllers/dragonControllers.js
@@ -8,6 +8,43 @@ const Dragons = require('../models/dragonModel.js');
 
 
 
+// ***************************************************************** //
+// **************************** ROUTES ***************************** //
+// ***************************************************************** //
+
+
+// -------------------- NEW ROUTE ------------------------ //
+
+router.get('/new', (req, res) => {
+	// This is where I want to render the new.ejs with a form that will be filled
+	res.render('new.ejs');
+});
+
+
+// -------------------- CREATE ROUTE --------------------- //
+
+router.post('/', (req, res) => {
+	// This is where I take the information gathered from the form and add it to our dragons database
+	console.log("-------------------- This is where the info gathered lives --------------------\n", req.body);
+
+	// Add gathered information to the database
+	Dragons.create(
+		req.body,
+		(err, createdDragon) => {
+			if (err) {console.log(`Error: ${err}`);}
+			else {
+				console.log("-------------------- Created Dragon --------------------\n", createdDragon);
+				res.redirect('/dragons');
+			}
+		})
+});
+
+
+// -------------------- INDEX ROUTE ---------------------- //
+
+router.get('/', (req, res) => {
+	
+})
 
 
 
@@ -22,3 +59,4 @@ const Dragons = require('../models/dragonModel.js');
 
 
 module.exports = router;
+
--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-// -------------------- REQUIRE EXPRESS -------------------- //
+// -------------------- REQUIRE EXPRESS ------------------------------- //
 const express = require('express');
 const app = express();
 
 
-// -------------------- SET PORT -------------------- //
-const PORT = 3000;
-app.listen(PORT, () => {
-	console.log(`Server listening on port ${PORT}`);
-});
-
-
-// -------------------- REQUIRE DATABASE -------------------- //
+// -------------------- REQUIRE DATABASE ------------------------------ //
 require('./db/db.js');
 
 
-// -------------------- MIDDLEWARE -------------------- //
+// -------------------- MIDDLEWARE/CONTROLLERS ------------------------ //
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const dragonController = require('./controllers/dragonControllers.js');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
-
-
+app.use('/dragons', dragonController);
 
 
 app.get('/', (req, res) => {
 	res.send(`Server responding to req from app`);
+});
+
+
+// -------------------- SET PORT -------------------------------------- //
+const PORT = 3000;
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`);
 });
